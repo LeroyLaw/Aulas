c2fc9a04773257e7028044378d13ec6fd5afbfb2
LeroyLaw <wk.scbd@gmail.com>
2018-10-25 15:37:38 -0300

    Aulas

---
+++ aula06a.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um Número = '))
+n2 = int(input('Digite outro Número = '))
+s = n1+n2
+# print ('A soma entre', n1, 'e', n2, 'vale:', s) ###formato do python2###
+print('A soma entre {} e {} vale: {}'.format(n1, n2, s))
+
---
+++ aula07a.py
@@ -0,0 +1,13 @@
+##nome = input('Qual é seu nome? ')
+##print ('É um prazer em te conhecer {:^20}!' .format(nome))
+
+n1=int(input('Digite um valor= '))
+n2=int(input('Digite outro valor= '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+print('A soma é {}, a multiplicação é {}, a divisão é {:.3f}' .format(s, m, d))
+print('Divisão inteira é {}, e potência é {}'. format(di, e))
