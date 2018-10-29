e91614124f7a37b1165f573f385b88be88330bac
LeroyLaw <wk.scbd@gmail.com>
2018-10-29 17:59:20 -0300

    Added to Aulas

---
+++ aula08a.py
@@ -0,0 +1,13 @@
+#import math
+#n = int(input('Digite um número: '))
+#r = math.sqrt(n)
+#print('A raiz de {} é igual a {.:2f}!'.format(n, r))
+
+#from math import sqrt, floor
+#n = int(input('Digite um número: '))
+#r = sqrt(n)
+#print('A raiz de {} é igual a {:.2f}!'.format(n, floor(r)))
+
+import random
+n = random.random()
+print(n)
